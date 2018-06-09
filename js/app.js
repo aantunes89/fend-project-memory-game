79c6ead007cff91d82dc5e564ed2a375a6c3fddc
Andre Antunes <andrepalhares1989@gmail.com>
2018-06-08 22:10:02 -0300

    START REFACTORING (READ DESCRIPTION)
    I've started refactoring the code, to make it more reusable.
    Create and set the cards in the deck (without shuffling it).
    Add flip event listener to cards (as a anonimous function,
    I will create a separete on and call it inside the eventListener).

--- js/app.js
+++ js/app.js
@@ -1,131 +1,131 @@
 /*
- * Create a list that holds all of your cards
+ * CREATE AND SET CARDS
  */
-let cardsArray = ['fa-diamond','fa-paper-plane-o','fa-anchor',
-'fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb',
-'fa-diamond','fa-paper-plane-o','fa-anchor',
-'fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb'];
+
+let icons = ["fa fa-diamond","fa fa-diamond","fa fa-paper-plane-o","fa fa-paper-plane-o",
+"fa fa-anchor","fa fa-anchor","fa fa-bolt","fa fa-bolt",
+"fa fa-cube","fa fa-cube","fa fa-leaf","fa fa-leaf",
+"fa fa-bicycle","fa fa-bicycle","fa fa-bomb","fa fa-bomb",];
 
 let deck = document.querySelector(".deck");
 
-let cardClass = document.getElementsByClassName("card");
+let cards = document.getElementsByClassName("card");
 
-let flippedCards = [];
-
-let matched = [];
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-startGame()
-
-function startGame(){
-    matched = [];
-    flippedCards = [];
-    shuffle(cardsArray);
-
-    for(let i = 0 ; i < cardsArray.length ; i++){
+function setCards() {
+    for(let i = 0; i < icons.length; i++ ) {
+        let liCard = document.createElement("li");
+        liCard.classList.add("card");
+        liCard.innerHTML = " <i class='" + icons[i] + "'></i>";
         
-        let liElement = document.createElement('li');
-        let iElement = document.createElement('i');
-
-        liElement.classList.add('card');
-        iElement.classList.add('fa');
-        iElement.classList.add(cardsArray[i]);
-
-        liElement.appendChild(iElement);
-        deck.appendChild(liElement);
-
-        cardClass[i].addEventListener('click', flipCard, false);
+        deck.appendChild(liCard);
+        cards[i].addEventListener("click", function(){
+            this.classList.add('open', 'show');
+        });
     }
-};
+}
 
-function flipCard(){
-    if(flippedCards.length < 2){
+setCards();
 
-        if(this.classList.length > 1){
-            return;
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+// let flippedCards = [];
+
+// let matched = [];
+
+
+// function flipCard(){
+//     if(flippedCards.length < 2){
+
+//         if(this.classList.length > 1){
+//             return;
+//         }
         
-        this.classList.add('open','show');
+//         this.classList.add('open','show');
 
-        flippedCards.push(this);
+//         flippedCards.push(this);
 
-        match();
-    }
-}
+//         match();
+//     }
+// }
 
-function match(){
-    if (flippedCards.length === 2){
-        if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
-            flippedCards[0].classList.toggle('match');
-            flippedCards[1].classList.toggle('match');
+// function match(){
+//     if (flippedCards.length === 2){
+//         if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
+//             flippedCards[0].classList.toggle('match');
+//             flippedCards[1].classList.toggle('match');
 
-            matched.push(flippedCards[0],flippedCards[1]);
-            flippedCards = [];
-        }
-        else{
-            missed();
-        }
-        movesCounter()
-    }
-    gameOver();
-}
+//             matched.push(flippedCards[0],flippedCards[1]);
+//             flippedCards = [];
+//         }
+//         else{
+//             missed();
+//         }
+//         movesCounter()
+//     }
+//     gameOver();
+// }
 
 
-function missed(){
-    setTimeout(function(){
-        flippedCards[0].classList.toggle('open');
-        flippedCards[0].classList.toggle('show');
-        flippedCards[1].classList.toggle('open');
-        flippedCards[1].classList.toggle('show');
+// function missed(){
+//     setTimeout(function(){
+//         flippedCards[0].classList.toggle('open');
+//         flippedCards[0].classList.toggle('show');
+//         flippedCards[1].classList.toggle('open');
+//         flippedCards[1].classList.toggle('show');
         
-        flippedCards = [];
-    },1000);
-}
+//         flippedCards = [];
+//     },1000);
+// }
 
-let counter = 0;
-const moves = document.querySelector(".moves");
-moves.innerHTML = 0;
-function movesCounter(){
-    counter++;
-    moves.innerHTML = counter;
-}
+// let counter = 0;
+// const moves = document.querySelector(".moves");
+// moves.innerHTML = 0;
+// function movesCounter(){
+//     counter++;
+//     moves.innerHTML = counter;
+// }
 
-function gameOver() {
-    if(matched.length === cardsArray.length) {
-        setTimeout(function(){
-            alert('GAME OVER!!');
-        },800);
-    }
-}
+// function gameOver() {
+//     if(matched.length === icons.length) {
+//         setTimeout(function(){
+//             alert('GAME OVER!!');
+//         },800);
+//     }
+// }
 
-//RESTART FUNCTION
-const restart = document.querySelector('.restart');
-restart.addEventListener('click', function() {
+// //RESTART FUNCTION
+// const restart = document.querySelector('.restart');
+// restart.addEventListener('click', function() {
     
-    deck.innerHTML = "";
-    startGame();
-    matched = [];
-    counter = 0;
-    moves.innerHTML = counter;
-});
+//     deck.innerHTML = "";
+//     startGame();
+//     matched = [];
+//     counter = 0;
+//     moves.innerHTML = counter;
+// });
+
+
+// // Shuffle function from http://stackoverflow.com/a/2450976
+// function shuffle(array) {
+//     var currentIndex = array.length, temporaryValue, randomIndex;
+
+//     while (currentIndex !== 0) {
+//         randomIndex = Math.floor(Math.random() * currentIndex);
+//         currentIndex -= 1;
+//         temporaryValue = array[currentIndex];
+//         array[currentIndex] = array[randomIndex];
+//         array[randomIndex] = temporaryValue;
+//     }
+
+//     return array;
+// }
