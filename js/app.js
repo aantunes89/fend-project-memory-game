9158ca9690d2d8e0e1905ab3079b7c31f951c9c0
Andre Antunes <andrepalhares1989@gmail.com>
2018-06-12 20:31:06 -0300

    Organized code

--- js/app.js
+++ js/app.js
@@ -28,15 +28,141 @@ let minutes = 0;
 let seconds = 0;
 let interval;
 
+// MODAL VARIABLES
 let overlay = document.querySelector(".overlay");
+
+
 // ----FUNCTIONS----- //
 
 
 
 /*
- *  SET CARDS  
+ *  RATING FUNCTION 
+ */
+// -----> REMOVES STARS
+function rating() {
+    if(move > 12){
+        stars[0].classList.remove("fa-star");
+        if (move > 18) {
+            stars[1].classList.remove("fa-star");
+        }
+    } 
+}
+
+// -----> STARTING RATING
+function startingRating() {
+    stars[0].classList.add("fa-star");
+    stars[1].classList.add("fa-star");
+}
+
+/*
+ *  TIMER
+ */
+function starTime() {
+    minutes = 0;
+    seconds = 0;
+    hour = 0;
+    timer.innerHTML = "Time "+ minutes +" mins : " + seconds +" secs";
+    clearInterval(interval)
+}
+
+
+function clock() {
+    interval = setInterval(function(){
+        timer.innerHTML = "Time "+ minutes +" mins : " + seconds +" secs";
+        seconds++;
+
+        if(seconds === 60) {
+            minutes++;
+            seconds = 0
+        }
+        if(minutes === 60){
+            hour++;
+            minutes = 0;
+        }
+    },1000);
+    
+}
+
+
+function moveStart() {
+    if(move === 1){
+        seconds = 0;
+        minutes = 0;
+        hour = 0;
+        clock();
+    }
+}
+
+
+/*
+ *  COUNT MOVES (CALLS TIMER AND RATING)
  */
 
+
+function movesCounter() { 
+    move++;
+    counter.innerHTML = move;
+
+    moveStart();
+    // START RATING 
+    startingRating();
+    rating();
+}
+
+
+
+
+
+/*
+ *  RESTART GAME 
+ */
+ 
+function restart() {
+    deck.innerHTML = "";
+    init();
+
+    move = 0;
+    counter.innerHTML = 0;
+
+    starTime();
+}
+
+
+// ---- REFRES BUTTON ---- //
+function refreshBtn(){
+    const reset = document.querySelector(".restart");
+    reset.addEventListener("click",restart );
+}
+
+
+
+/*
+ *  GAME OVER
+ */
+function gameOver() {
+    setTimeout(function(){
+        if(matched.length === icons.length) {
+            congratulations();
+        }
+    },700);
+}
+
+
+function congratulations() {
+    overlay.style.opacity = "1";
+    overlay.style.zIndex = "2"
+}
+
+
+// -------------------------------------------- //
+//---------- CARDS RELATED FUNCTIONS ---------- //
+// -------------------------------------------- //
+
+
+/*
+ *  SET CARDS  
+ */
 function setCards() {
     for(let i = 0; i < icons.length; i++ ) {
         let liCard = document.createElement("li");
@@ -49,11 +175,9 @@ function setCards() {
 }
 
 
-
 /*
  *  UDACITY SHUFFLE FUNCTION 
  */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -70,12 +194,9 @@ function shuffle(array) {
 }
 
 
-
 /*
  *  SELECT CARDS 
  */
-
- 
 function selectCards() {
     if(selected.length < 2){
         if(this.classList.length > 1){
@@ -90,11 +211,9 @@ function selectCards() {
 } 
 
 
-
 /*
  *  MISSED TRY 
  */
- 
 function missed() {
     setTimeout(function missed(){
         selected[0].classList.remove("open" ,"show");
@@ -105,12 +224,9 @@ function missed() {
 }
 
 
-
 /*
  *  MATCH FUNCTION 
  */
-
-
 function match() {
     if(selected.length === 2){
         if(selected[0].innerHTML === selected[1].innerHTML) {
@@ -132,129 +248,17 @@ function match() {
 }
 
 
-
-/*
- *  COUNT MOVES 
- */
-
-
-function movesCounter() { 
-    move++;
-    counter.innerHTML = move;
-    if(move === 1){
-        seconds = 0;
-        minutes = 0;
-        hour = 0;
-        clock();
-    }
-    
-    // START RATING 
-    rating();
-}
-
-
-
-/*
- *  GAME OVER
- */
-
-function gameOver() {
-    setTimeout(function(){
-        if(matched.length === icons.length) {
-            alert("CONGRATULATIONS!!");
-            restart();
-            overlay.style.opacity = "1";
-
-        }
-    },700);
-}
-
-
-
-
-/*
- *  RESTART GAME 
- */
-
- // CAN BE REUSED!!!
-function restart() {
-    deck.innerHTML = "";
-    init();
-
-    move = 0;
-    counter.innerHTML = 0;
-    stars[0].classList.add("fa-star");
-    stars[1].classList.add("fa-star");
-    
-    minutes = 0;
-    seconds = 0;
-    hour = 0;
-    timer.innerHTML = "Time "+ minutes +" mins : " + seconds +" secs";
-    clearInterval(interval)
-}
-
-function refreshBtn(){
-    const reset = document.querySelector(".restart");
-    reset.addEventListener("click",restart );
-}
-
-
-
-/*
- *  RATING FUNCTION 
- */
-
-
-function rating() {
-    if(move > 12){
-        stars[0].classList.remove("fa-star");
-        if (move > 18) {
-            stars[1].classList.remove("fa-star");
-        }
-    } 
-}
-
-
-
-/*
- *  TIMER
- */
-
-
-function clock() {
-    interval = setInterval(function(){
-        timer.innerHTML = "Time "+ minutes +" mins : " + seconds +" secs";
-        seconds++;
-        console.log(seconds);
-        if(seconds === 60) {
-            minutes++;
-            seconds = 0
-        }
-        if(minutes === 60){
-            hour++;
-            minutes = 0;
-        }
-    },1000);
-    
-}
-
-
 /*
  * START GAME 
  */
 
 function init() {
-    timer.innerHTML = "Time "+ minutes +" mins : " + seconds +" secs";
     // shuffle(icons);
     setCards();
-    matched = [];
     refreshBtn();
+    timer.innerHTML = "Time "+ minutes +" mins : " + seconds +" secs";
+    matched = [];
 }
 
 init();
-
-
-
-
-
 
